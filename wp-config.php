416b67a80670155ff9d9fb41c8e33531cb02bd8b
phroggyy <leo.sjoberg@gmail.com>
2015-03-07 18:27:41 +0100

    fixed user page

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@ define('DB_USER', 'root');
 
 /** MySQL database password */
 //define('DB_PASSWORD', 'root');
-define('DB_PASSWORD', 'gfMqGneRoj')
+define('DB_PASSWORD', 'gfMqGneRoj');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
