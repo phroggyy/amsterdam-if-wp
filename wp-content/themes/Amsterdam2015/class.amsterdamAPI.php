4527d94caefb9fdfe02c21d90a0ff01959ba67ca
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-03-15 12:33:37 +0100

    Added task 1 implementation

---
+++ wp-content/themes/Amsterdam2015/class.amsterdamAPI.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leosjoberg
+ * Date: 07/03/15
+ * Time: 23:34
+ */
+
+class AmsterdamAPI {
+    private $baseUrl = 'http://api.tandem15.eu/';
+
+    private $key;
+    private $secret;
+
+    /**
+     * AmsterdamAPI constructor.
+     * @param $key
+     * @param $secret
+     */
+    public function __construct($key, $secret)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+    }
+
+
+    private function curl($url, $data = [])
+    {
+        $data['key'] = $this->key;
+        $data['secret'] = bcrypt($this->key . $this->secret);
+        $finalUrl = $this->baseUrl . $url;
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL, $finalUrl);
+        if (count($data) > 0) {
+            curl_setopt($ch,CURLOPT_POST, 1);
+            curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($data));
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    /* Methods for retrieving posts */
+    public function posts()
+    {
+        return $this->curl('posts');
+    }
+
+    public function post($id)
+    {
+        return $this->curl('posts/' . $id);
+    }
+    
+    /* Methods for retrieving material from task 1 */
+    public function taskOneByCommittee($committee)
+    {
+        return $this->curl('task1/' . $committee);
+    }
+
+    /* Methods for retrieving tickets */
+    public function tickets()
+    {
+        return $this->curl('tickets');
+    }
+
+    public function ticket($id)
+    {
+        return $this->curl('tickets/' . $id);
+    }
+
+    public function createTicket($data)
+    {
+        return ($this->curl('tickets', $data));
+    }
+}
---
+++ wp-content/themes/Amsterdam2015/template-task1.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Photos page
+*/
+get_header();
+
+$apiKey = 'lC7UO1LW75kSeyhaHfFB9IMnTuZPn9fdBtDTSTbcX76k1oLXdMp3ZvVFYdv5';
+$apiSecret = '1svooKg0SCigJ2pPG3RnfIWpJwlqzesiaEmV76iCnbAZwOBONOqg9JVnKQNM';
+$amsterdam = new AmsterdamAPI($apiKey, $apiSecret);
+
+
+?>
+
+<main role="main">
+  <div class="container">
+      <?php if (!isset($_GET['committee'])) : ?>
+          <section class="albums">
+              <?php foreach($albums as $album) { ?>
+                  <?php $bgPic = $fb->curl_get($album->cover_photo . '?fields=source'); $bgUrl = $bgPic->source; ?>
+                  <div class="single-album col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                      <div style="background-image: url(<?php echo $bgUrl; ?>);">
+                          <h2 class="text-center album-title"><a href="?album=<?php echo $album->id; ?>"><?php echo $album->name; ?></a></h2>
+                      </div>
+                  </div>
+              <?php } ?>
+          </section>
+          <?php else : $representations = $amsterdam->taskOneByCommittee($_GET['committee']); dd($representations); ?>
+          <section class="single-album">
+            <ul id="lightGallery">
+                <?php foreach ($pictures->data as $picture) : ?>
+                    <?php
+                    $size = count($picture->images);
+                    $small = $picture->images[$size-2]->source;
+                    $large = $picture->images[0]->source;
+                    ?>
+                    <li data-src="<?php echo $large; ?>"><img src="<?php echo $small; ?>" alt=""/></li>
+                <?php endforeach; ?>
+            </ul>
+          </section>
+      <?php endif; ?>
+  </div>
+</main>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#lightGallery").lightGallery();
+        });
+    </script>
+
+<?php get_footer(); ?>
