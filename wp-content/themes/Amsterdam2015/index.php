e77be3ee4b2909b915c53a9cf82be213c4e3afca
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-03-15 20:17:52 +0100

    fixed galleries

--- wp-content/themes/Amsterdam2015/index.php
+++ wp-content/themes/Amsterdam2015/index.php
@@ -3,11 +3,12 @@
 	<main role="main">
 
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fp-video">
-			<?php get_template_part('fpvideo'); ?>
+			<?php //get_template_part('fpvideo'); ?>
 		</section>
 		
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 articles">
-			<?php get_template_part('loop'); ?>
+			<?php //get_template_part('loop'); ?>
+			<p>Articles coming soon!</p>
 
 	</main>
 
