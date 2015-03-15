0c55b2246c49c95e3c39a6dc598acaebcadb7d58
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-03-15 19:21:17 +0100

    fixed gallery view

--- wp-content/themes/Amsterdam2015/template-task1.php
+++ wp-content/themes/Amsterdam2015/template-task1.php
@@ -76,7 +76,7 @@ $amsterdam = new AmsterdamAPI($apiKey, $apiSecret);
                     <?php
                     $src = 'http://task1.apps.tandem15.eu/uploads/' . $representation->representation;
                     ?>
-                    <li data-src="<?php echo $src; ?>"><img src="<?php echo $src; ?>" alt=""/></li>
+                    <li class="col-xs-6 col-md-3" data-src="<?php echo $src; ?>"><img class="col-xs-12" src="<?php echo $src; ?>" alt=""/></li>
                 <?php endforeach; ?>
             </ul>
               <?php else : ?>
