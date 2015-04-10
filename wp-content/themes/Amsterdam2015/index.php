d9340b2336ff7a1bdb34a4a3f5de3d0ddf66981e
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-04-10 16:35:46 +0200

    fixed first page video

--- wp-content/themes/Amsterdam2015/index.php
+++ wp-content/themes/Amsterdam2015/index.php
@@ -3,7 +3,8 @@
 	<main role="main">
 
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fp-video">
-			<?php get_template_part('fpvideo'); ?>
+			<?php // get_template_part('fpvideo'); ?>
+			<iframe src="https://player.vimeo.com/video/124613098" width="1280" height="720" frameborder="0" title="Welcome to Amsterdam2015!" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
 		</section>
 		
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 articles">
