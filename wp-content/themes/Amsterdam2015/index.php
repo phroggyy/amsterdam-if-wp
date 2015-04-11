3c0171fc946152a47b7af520ac76ae13334d7ea6
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-04-11 14:35:06 +0200

    changed first page video

--- wp-content/themes/Amsterdam2015/index.php
+++ wp-content/themes/Amsterdam2015/index.php
@@ -4,7 +4,8 @@
 
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fp-video">
 			<?php // get_template_part('fpvideo'); ?>
-			<iframe src="https://player.vimeo.com/video/124613098" width="1280" height="720" frameborder="0" title="Welcome to Amsterdam2015!" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+<!--			<iframe src="https://player.vimeo.com/video/124613098" width="1280" height="720" frameborder="0" title="Welcome to Amsterdam2015!" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>-->
+			<iframe src="https://player.vimeo.com/video/124692482" width="1920" height="1080" frameborder="0" title="Amsterdam2015 Party" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
 		</section>
 		
 		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 articles">
