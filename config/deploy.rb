65328a617738494525c201e6f57771b1d81587d2
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-04-12 10:16:08 +0200

    updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['wp-content/uploads', '.htaccess']
+set :shared_paths, ['wp-content/uploads', '.htaccess', 'php5.ini']
 
 # Optional settings:
     set :user, 'root'    # Username in the server to SSH to.
